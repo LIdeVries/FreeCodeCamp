53e50fa8af8187264518b210aef598bf6514ac99
LIdeVries <87062099+LIdeVries@users.noreply.github.com>
2022-11-25 07:58:38 +0200

    play.ipynb updated, and showing Mikaela git

--- Data Analysis With Python/Medical_data_visualiser.py
+++ Data Analysis With Python/Medical_data_visualiser.py
@@ -4,48 +4,87 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Import data
-df = pd.read_csv("medical_examination.csv", index_col="id")
+df = pd.read_csv(
+    "medical_examination.csv",
+    index_col="id",
+)
 
 # Add 'overweight' column
-df["overweight"] = None
+bmi = df["weight"] / ((df["height"] / 100) ** 2)
+df["overweight"] = bmi > 25
 
 # Normalize data by making 0 always good and 1 always bad. If the value of 'cholesterol' or 'gluc' is 1, make the value 0. If the value is more than 1, make the value 1.
-
+df["cholesterol"] = [1 if x > 1 else 0 for x in df["cholesterol"]]
+df["gluc"] = [1 if x > 1 else 0 for x in df["gluc"]]
 
 # Draw Categorical Plot
 def draw_cat_plot():
     # Create DataFrame for cat plot using `pd.melt` using just the values from 'cholesterol', 'gluc', 'smoke', 'alco', 'active', and 'overweight'.
-    df_cat = None
+    df_cat = pd.melt(
+        df,
+        id_vars=["cardio"],
+        value_vars=["cholesterol", "gluc", "smoke", "alco", "active", "overweight"],
+    )
 
     # Group and reformat the data to split it by 'cardio'. Show the counts of each feature. You will have to rename one of the columns for the catplot to work correctly.
-    df_cat = None
+    df_cat = df_cat.groupby(["cardio", "variable", "value"], as_index=False).count()
+    df_cat.rename(columns={"count": "total"}, inplace=True)
 
     # Draw the catplot with 'sns.catplot()'
-
-    # Get the figure for the output
-    fig = None
+    fig = sns.heatmap(
+        corr,
+        mask=mask,
+        fmt=".1f",
+        vmax=0.3,
+        linewidths=0.5,
+        square=True,
+        cbar_kws={"shrink": 0.5},
+        annot=True,
+        center=0,
+    )
 
     # Do not modify the next two lines
     fig.savefig("catplot.png")
     return fig
 
 
+draw_cat_plot()
+
 # Draw Heat Map
 def draw_heat_map():
     # Clean the data
-    df_heat = None
-
+    df_heat = df.loc[
+        (df["ap_lo"] <= df["ap_hi"])
+        & (df["height"] >= df["height"].quantile(0.025))
+        & (df["weight"] >= df["weight"].quantile(0.025))
+        & (df["height"] <= df["height"].quantile(0.975))
+        & (df["weight"] <= df["weight"].quantile(0.975))
+    ]
     # Calculate the correlation matrix
-    corr = None
+    corr = round(df_heat.corr(), 1)
 
     # Generate a mask for the upper triangle
-    mask = None
+    mask = np.triu(corr)
 
     # Set up the matplotlib figure
-    fig, ax = None
+    fig, ax = plt.subplots(figsize=(11, 9))
 
     # Draw the heatmap with 'sns.heatmap()'
+    sns.heatmap(
+        corr,
+        mask=mask,
+        cbar_kws={"shrink": 0.5},
+        center=0.0,
+        annot=True,
+        fmt=".1f",
+        linewidths=0.5,
+        vmax=0.32,
+        vmin=-0.16,
+    )
 
     # Do not modify the next two lines
     fig.savefig("heatmap.png")
     return fig
+
+    # Do not modify the next two lines
+    fig.savefig("heatmap.png")
