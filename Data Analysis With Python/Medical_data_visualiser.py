d1b2b0c6d7eab53acea01df588098ac640633f84
LIdeVries <87062099+LIdeVries@users.noreply.github.com>
2022-11-25 09:25:12 +0200

    Medical_data_visualiser.py complete

--- Data Analysis With Python/Medical_data_visualiser.py
+++ Data Analysis With Python/Medical_data_visualiser.py
@@ -1,90 +1,71 @@
-import pandas as pd
-import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+import seaborn as sns
 
 # Import data
 df = pd.read_csv(
     "medical_examination.csv",
-    index_col="id",
 )
 
 # Add 'overweight' column
-bmi = df["weight"] / ((df["height"] / 100) ** 2)
-df["overweight"] = bmi > 25
+df["overweight"] = (df["weight"] / (df["height"] / 100) ** 2 > 25).astype(int)
 
-# Normalize data by making 0 always good and 1 always bad. If the value of 'cholesterol' or 'gluc' is 1, make the value 0. If the value is more than 1, make the value 1.
-df["cholesterol"] = [1 if x > 1 else 0 for x in df["cholesterol"]]
-df["gluc"] = [1 if x > 1 else 0 for x in df["gluc"]]
+# Normalize data by making 0 always good and 1 always bad. If the value of 'cholestorol' or 'gluc' is 1,
+# make the value 0. If the value is more than 1, make the value 1.
+df["cholesterol"] = (df["cholesterol"] > 1).astype(int)
+df["gluc"] = (df["gluc"] > 1).astype(int)
+
 
 # Draw Categorical Plot
 def draw_cat_plot():
-    # Create DataFrame for cat plot using `pd.melt` using just the values from 'cholesterol', 'gluc', 'smoke', 'alco', 'active', and 'overweight'.
+    # Create DataFrame for cat plot using `pd.melt` using just the values from 'cholesterol', 'gluc', 'smoke',
+    # 'alco', 'active', and 'overweight'.
     df_cat = pd.melt(
         df,
         id_vars=["cardio"],
         value_vars=["cholesterol", "gluc", "smoke", "alco", "active", "overweight"],
     )
 
-    # Group and reformat the data to split it by 'cardio'. Show the counts of each feature. You will have to rename one of the columns for the catplot to work correctly.
-    df_cat = df_cat.groupby(["cardio", "variable", "value"], as_index=False).count()
-    df_cat.rename(columns={"count": "total"}, inplace=True)
+    # Group and reformat the data to split it by 'cardio'. Show the counts of each feature. You will have to rename
+    # one of the collumns for the catplot to work correctly.
+    df_cat = df_cat.groupby(["cardio", "variable", "value"]).size().reset_index()
+    df_cat = df_cat.rename(columns={0: "total"})
 
     # Draw the catplot with 'sns.catplot()'
-    fig = sns.heatmap(
-        corr,
-        mask=mask,
-        fmt=".1f",
-        vmax=0.3,
-        linewidths=0.5,
-        square=True,
-        cbar_kws={"shrink": 0.5},
-        annot=True,
-        center=0,
+    graph = sns.catplot(
+        data=df_cat, kind="bar", x="variable", y="total", hue="value", col="cardio"
     )
+    fig = graph.fig
 
     # Do not modify the next two lines
     fig.savefig("catplot.png")
     return fig
 
 
-draw_cat_plot()
-
 # Draw Heat Map
 def draw_heat_map():
     # Clean the data
-    df_heat = df.loc[
+    df_heat = df[
         (df["ap_lo"] <= df["ap_hi"])
         & (df["height"] >= df["height"].quantile(0.025))
         & (df["weight"] >= df["weight"].quantile(0.025))
         & (df["height"] <= df["height"].quantile(0.975))
         & (df["weight"] <= df["weight"].quantile(0.975))
     ]
+
     # Calculate the correlation matrix
-    corr = round(df_heat.corr(), 1)
+    corr = df_heat.corr()
 
     # Generate a mask for the upper triangle
-    mask = np.triu(corr)
+    mask = np.triu(np.ones_like(corr, dtype=bool))
 
     # Set up the matplotlib figure
-    fig, ax = plt.subplots(figsize=(11, 9))
+    fig, ax = plt.subplots(figsize=(12, 6))
 
     # Draw the heatmap with 'sns.heatmap()'
-    sns.heatmap(
-        corr,
-        mask=mask,
-        cbar_kws={"shrink": 0.5},
-        center=0.0,
-        annot=True,
-        fmt=".1f",
-        linewidths=0.5,
-        vmax=0.32,
-        vmin=-0.16,
-    )
+    sns.heatmap(corr, mask=mask, square=True, linewidths=0.5, annot=True, fmt="0.1f")
 
     # Do not modify the next two lines
     fig.savefig("heatmap.png")
     return fig
-
-    # Do not modify the next two lines
-    fig.savefig("heatmap.png")
